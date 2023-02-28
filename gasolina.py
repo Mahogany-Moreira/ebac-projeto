e06928bc4efa960c4375f5111f4eb1a145db5aee
Mahogany-Moreira <hudhammahogany@gmail.com>
2023-02-28 04:39:41 +0000

    Adicionando mais 3 arquivos ao projeto na nova branch

--- gasolina.py
+++ gasolina.py
@@ -1,8 +1,8 @@
-# código de geração do gráfico 
+# código de geração do gráfico
 import pandas as pd
 import seaborn as sns
 
 gasolina_df = pd.read_csv('gasolina.csv', sep=',')
 
-grafico = sns.lineplot(data=gasolina_df, x="dia", y="venda", palette="pastel")
-grafico.set(title='Preço da gasolina por dia (BRL)', xlabel='Dia da semana', ylabel='Preço (BRL)');
+grafico = sns.lineplot(data=gasolina_df, x="dia", y="venda", color="red")
+grafico.set(title='Gasolina: Preço por dia (BRL)', xlabel='Dia da semana', ylabel='Preço (BRL)')
